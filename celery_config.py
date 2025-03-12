e03a802098a48f8ba12874a0173359e249036cd5
Luna-ris <peimonqqq@gmail.com>
2025-03-13 01:17:06 +0300

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -2,7 +2,7 @@ from celery import Celery
 import os
 
 # Настройка Celery с использованием REDIS_URL от Railway
-app = Celery('tasks', broker=os.getenv('redis://default:${{REDIS_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}', 'redis://localhost:6379/0'))
+app = Celery('tasks', broker=os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
 app.conf.task_serializer = 'json'
 app.conf.result_serializer = 'json'
 app.conf.accept_content = ['json']
