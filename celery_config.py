5bca2d31eab877393da319ad573536634898f6a0
Luna-ris <peimonqqq@gmail.com>
2025-03-18 17:35:41 +0300

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -2,12 +2,10 @@ import os
 from celery import Celery
 
 
-# Настройка Celery с использованием REDIS_URL от Railway
-app = Celery('tasks', broker=os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
-
-# Конфигурация Celery
-app.conf.task_serializer = 'json'
-app.conf.result_serializer = 'json'
-app.conf.accept_content = ['json']
-app.conf.timezone = 'UTC'
-app.conf.broker_connection_retry_on_startup = True
+# Настройка Celery
+celery_app = Celery('tasks', broker=os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
+celery_app.conf.task_serializer = 'json'
+celery_app.conf.result_serializer = 'json'
+celery_app.conf.accept_content = ['json']
+celery_app.conf.timezone = 'UTC'
+celery_app.conf.broker_connection_retry_on_startup = True
