b560e5e4649390a8fc58972ffe89cef2f01bf74a
Luna-ris <peimonqqq@gmail.com>
2025-03-18 19:07:13 +0300

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -5,5 +5,5 @@ celery_app = Celery('tasks', broker=os.getenv('REDIS_URL', 'redis://localhost:63
 celery_app.conf.task_serializer = 'json'
 celery_app.conf.result_serializer = 'json'
 celery_app.conf.accept_content = ['json']
-celery_app.conf.timezone = 'UTC' 
+celery_app.conf.timezone = 'Europe/Moscow'  # Установите часовой пояс Москвы
 celery_app.conf.broker_connection_retry_on_startup = True
