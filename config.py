64ca27c1e74c11eb423d1476d5729fc84e69c2d6
Luna-ris <peimonqqq@gmail.com>
2025-03-19 21:41:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import logging
 from dotenv import load_dotenv
 from supabase import create_client
 from celery import Celery
+from database import generate_unique_capsule_number
 
 # Настройка логирования
 logger = logging.getLogger(__name__)
