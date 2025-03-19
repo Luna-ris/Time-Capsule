560da19b6798ce913fa51c8a3014bc39dfc5a616
Luna-ris <peimonqqq@gmail.com>
2025-03-19 19:37:00 +0300

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -4,21 +4,21 @@ from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 from config import ENCRYPTION_KEY_BYTES
 
-def encrypt_data_aes(data: str, key: bytes = ENCRYPTION_KEY_BYTES) -> str:
+def encrypt_data_aes(data: str) -> str:
     """Шифрование данных с помощью AES."""
     iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(ENCRYPTION_KEY_BYTES), modes.CBC(iv), backend=default_backend())
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(data.encode('utf-8')) + padder.finalize()
     encryptor = cipher.encryptor()
     encrypted = encryptor.update(padded_data) + encryptor.finalize()
     return (iv + encrypted).hex()
 
-def decrypt_data_aes(encrypted_hex: str, key: bytes = ENCRYPTION_KEY_BYTES) -> str:
+def decrypt_data_aes(encrypted_hex: str) -> str:
     """Дешифрование данных с помощью AES."""
     data = bytes.fromhex(encrypted_hex)
     iv, encrypted = data[:16], data[16:]
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(ENCRYPTION_KEY_BYTES), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     decrypted = decryptor.update(encrypted) + decryptor.finalize()
     unpadder = padding.PKCS7(128).unpadder()
