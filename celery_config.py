0c0e99a76110d377b97b586ec6f02e46f3715b42
Luna-ris <peimonqqq@gmail.com>
2025-04-01 18:51:18 +0300

    Delete celery_config.py

--- celery_config.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-import os
-
-celery_app = Celery('tasks', broker=os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
-celery_app.conf.task_serializer = 'json'
-celery_app.conf.result_serializer = 'json'
-celery_app.conf.accept_content = ['json']
-celery_app.conf.timezone = 'Europe/Moscow'  # Установите часовой пояс Москвы
-celery_app.conf.broker_connection_retry_on_startup = True
